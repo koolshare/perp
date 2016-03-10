71f14dfda77916f6c9220c9518a7c5abc10d5be6
janson <gandancing@gmail.com>
2016-03-10 22:22:22 +0800

    build ok in merlin

---
+++ conf.mk
@@ -0,0 +1,25 @@
+# conf.mk
+# project build/install configs
+# wcm, 2009.09.14 - 2013.01.08
+# ===
+
+## build configuration, standard gcc + libc:
+#CC = gcc
+CC=arm-brcm-linux-uclibcgnueabi-gcc
+CFLAGS = -Wall -Wextra -Wshadow -DNDEBUG -O2
+#CFLAGS = -Wall -Wextra -Wshadow -O2
+
+## build configuration, dietlibc:
+#CC = diet -Os gcc
+#CFLAGS = -Wall -Wextra -Wshadow -DNDEBUG
+
+## strip configuration
+STRIP = strip
+#STRIP = /some/other/stripper
+
+## install configuration:
+BINDIR = /usr/bin
+SBINDIR = /usr/sbin
+MANDIR  = /usr/share/man
+
+### EOF (conf.mk)
